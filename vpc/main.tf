0ea110834a8eaaf001985d76d517c924ae42e12f
owen0409 <owen0409@163.com>
2021-08-14 12:35:34 +0800

    create vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,4 +1,7 @@
-provider "google-beta" {
+provider "google" {
+  credentials = file("/home/owen0409/organic-cat-322902-b1faae0ead35.json")
+  project     = "organic-cat-322902"
+  region      = "us-west1"
 }
 
 resource "google_compute_network" "vpc_network" {
