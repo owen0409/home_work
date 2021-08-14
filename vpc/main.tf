32b93f0efd7ef25336516fb9134b5f15b253bfa1
owen0409 <owen0409@163.com>
2021-08-14 12:16:07 +0800

    create vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project     = var.project_id
+  ##region      = "us-central1"
+}
+
 resource "google_compute_network" "vpc_network" {
   project                 = var.project_id
   name                    = var.vpc_network_name
