afdf678317573bf0d1d13454d5cf417971f79a33
owen0409 <owen0409@163.com>
2021-08-14 12:12:01 +0800

    create vpc

---
+++ vpc/main.tf
@@ -0,0 +1,6 @@
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project_id
+  name                    = var.vpc_network_name
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  type = string
+  default = "organic-cat-322902"
+}
+variable "vpc_network_name" {
+  type = string
+  default = "test_vpc"
+}
