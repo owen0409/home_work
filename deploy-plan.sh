8ebc5e900a7a15b4c5577c025e6f12b44943848d
owen0409 <owen0409@163.com>
2021-08-14 19:46:18 +0800

    add pull

--- deploy-plan.sh
+++ deploy-plan.sh
@@ -1,2 +1,3 @@
+git pull
 terragrunt plan-all  --terragrunt-non-interactive  --terragrunt-working-dir deploy/vpc
 terragrunt plan-all  --terragrunt-non-interactive  --terragrunt-working-dir deploy/test-vm-1
