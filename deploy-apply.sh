05f42aeaa80815c1f224b4ebb0e9dbdb4a3e2be3
owen0409 <owen0409@163.com>
2021-08-14 19:45:03 +0800

    apply vm

--- deploy-apply.sh
+++ deploy-apply.sh
@@ -1,1 +1,2 @@
 terragrunt apply-all  --terragrunt-non-interactive --terragrunt-working-dir deploy/vpc
+terragrunt apply-all  --terragrunt-non-interactive --terragrunt-working-dir deploy/test-vm-1
--- deploy-plan.sh
+++ deploy-plan.sh
@@ -1,3 +1,2 @@
 terragrunt plan-all  --terragrunt-non-interactive  --terragrunt-working-dir deploy/vpc
-# sleep 5
-# terragrunt plan-all  --terragrunt-non-interactive  --terragrunt-working-dir deploy/test-vm-1
+terragrunt plan-all  --terragrunt-non-interactive  --terragrunt-working-dir deploy/test-vm-1
