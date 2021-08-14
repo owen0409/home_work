0e29202e5e5e79d6cb72e7dfbc4f28285f646208
owen0409 <owen0409@163.com>
2021-08-14 21:25:55 +0800

    add repo

--- VM/script/test.sh
+++ VM/script/test.sh
@@ -1,10 +1,10 @@
 yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
 yum -y install docker-ce
 service docker start
 sudo docker run  --rm --name nginx -d -p 80:80 nginx:latest
 
-firewall-cmd --permanent --add-port=80/tcp
-firewall-cmd --reload
-firewall-cmd --list-all
+# firewall-cmd --permanent --add-port=80/tcp
+# firewall-cmd --reload
+# firewall-cmd --list-all
