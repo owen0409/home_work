f1bad13f973fac161dfa5938f6e0520a6b30fabd
owen0409 <owen0409@163.com>
2021-08-14 15:26:45 +0800

    create vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,7 +4,7 @@ provider "google-beta" {
 resource "google_compute_network" "vpc_network" {
   project                 = var.project_id
   name                    = var.vpc_network_name
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
   mtu                     = 1460
 }
 
@@ -16,7 +16,7 @@ resource "google_compute_network" "default" {
 resource "google_compute_subnetwork" "default" {
   project                 = var.project_id
   name          = "test-subnet"
-  ip_cidr_range = "10.170.0.2/28"
+  ip_cidr_range = "10.170.0.0/24"
   region        = "asia-east2"
   network       = google_compute_network.default.id
 }
