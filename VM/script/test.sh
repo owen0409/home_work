bccd8f3b60ecc3c99777eb17cc3650ba35ab2993
owen0409 <owen0409@163.com>
2021-08-14 18:21:45 +0800

    com

--- VM/script/test.sh
+++ VM/script/test.sh
@@ -4,3 +4,7 @@ yum makecache fast
 yum -y install docker-ce
 service docker start
 sudo docker run  --rm --name nginx -d -p 80:80 nginx:latest
+
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --reload
+firewall-cmd --list-all
